c428b2cfe0dc555241e95f1bc1133661c2811a6a
Liam Ostrander <119699311+figgythenewton@users.noreply.github.com>
2025-03-15 22:43:22 -0400

    Initialized pygame window

---
+++ window.py
@@ -0,0 +1,28 @@
+# Example file showing a basic pygame "game loop"
+import pygame
+import math
+
+# pygame setup
+pygame.init()
+screen = pygame.display.set_mode((640, 360))
+clock = pygame.time.Clock()
+running = True
+
+while running:
+    # poll for events
+    # pygame.QUIT event means the user clicked X to close your window
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    # fill the screen with a color to wipe away anything from last frame
+    screen.fill("purple")
+
+    # RENDER YOUR GAME HERE
+    screen.set_at(), "Black"
+    # flip() the display to put your work on screen
+    pygame.display.flip()
+
+    clock.tick(60)  # limits FPS to 60
+
+pygame.quit()
